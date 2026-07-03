5ee819bbc3f5c9cbd9184e1ab99494e7a0fe5b09
Aohgga <96177323+Aohgga@users.noreply.github.com>
2026-07-03 18:56:56 +0800

    Update default.nix

--- maple-mono.NFPropo-CN/default.nix
+++ maple-mono.NFPropo-CN/default.nix
@@ -2,10 +2,11 @@
 
 stdenv.mkDerivation {
   pname = "maple-mono.NFPropo-CN";
-  version = "1.0.0";
+  version = "7.9";
   src = fetchzip {
     url = "https://github.com/Aohgga/maple-font/releases/download/v1776757239/MapleMono-NF-CN.zip";
     hash = "sha256-qUDQD8+iy7k7JDsKT5rbk5eCGO9cUfMxZtIERm00kHo=";
+    stripRoot = false;
   };
 
   installPhase = ''
