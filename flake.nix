7572808b084f9c8f76ff4f89b19e937d62ce26ce
Aohgga <96177323+Aohgga@users.noreply.github.com>
2026-07-03 21:35:41 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let 
-    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" ];
-    in {
-    packages = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages."${system}";
+  outputs = {
+    nixpkgs,
+    ...
+  }:
+  let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "i686-linux"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    packages = forAllSystems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
       in {
         maple-mono.NFPropo-CN = pkgs.callPackage ./maple-mono.NFPropo-CN {};
-      });
-    };
+      }
+    );
+  };
 }
