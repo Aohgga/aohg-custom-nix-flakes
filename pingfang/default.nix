fb44b3ee66fe87cf23c107a35ab3c8adf6dc7150
Aohgga <96177323+Aohgga@users.noreply.github.com>
2026-07-04 16:10:52 +0800

    Update default.nix

--- pingfang/default.nix
+++ pingfang/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   src = fetchzip {
     url = "https://github.com/witt-bit/applePingFangFonts/releases/download/3.0.1/pingFang-20.0d4e1.tar.gz";
     hash = "sha256-M8vBp41XIQLEv8ZNArRbZg7fwECVk5rYolfU4/Eh5T4=";
-    stripRoot = false;
+    stripRoot = true;
   };
 
   installPhase = ''
